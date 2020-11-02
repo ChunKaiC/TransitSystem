ec80f26e423fb347dfb2eb4035f2863bcd272c8e
Nicholas <stug4444@hotmail.com>
2020-11-01 22:55:53 -0500

    Created Admin User class not fully implemented just the template

---
+++ src/transitapp/AdminUser.java
@@ -0,0 +1,12 @@
+package transitapp;
+
+public class AdminUser {
+	private static double totalRevenue;
+	private static double totalCost;
+	
+	public double showDailyReport() {
+		return 0.0;
+	}
+	
+
+}
