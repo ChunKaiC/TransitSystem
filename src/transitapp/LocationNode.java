300fa20c53de8f019344cc9368cb874cfe5ebecd
Kai <kai@chen.com>
2020-11-02 02:49:52 -0500

    Added LocationNode.java

---
+++ src/transitapp/LocationNode.java
@@ -0,0 +1,11 @@
+package transitapp;
+
+public class LocationNode {
+	private LocationNode next;
+	private LocationNode previous;
+	private  String item;
+	
+    public static void main(String[] args) {
+
+    }
+}
