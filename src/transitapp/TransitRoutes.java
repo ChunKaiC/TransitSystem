14bd4f3c064599a393967420f258f050fb9277c0
Kai <kai@chen.com>
2020-11-04 16:20:49 -0500

    Changed instance variable type

--- src/transitapp/TransitRoutes.java
+++ src/transitapp/TransitRoutes.java
@@ -1,8 +1,15 @@
 package transitapp;
 
+import java.util.ArrayList;
+
 public class TransitRoutes {
-	private LocationNode nodes;
-	private int fares;
+	private ArrayList<Object> route;
+	private double fare;
+	
+	public TransitRoutes(ArrayList<Object> route, double fare) {
+		this.route = route;
+		this.fare = fare;
+	}
 	
     public static void main(String[] args) {
 
