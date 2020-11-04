0d658f3cb09ff6369aa846f2711efd99ff9540b2
visanaxel <axel.visan@mail.utoronto.ca>
2020-11-04 11:51:04 -0500

    Updated tapOn and tapOff methods - incomplete yet

--- src/transitapp/Card.java
+++ src/transitapp/Card.java
@@ -3,12 +3,14 @@ package transitapp;
 public class Card {
 
     private double balance;
-    private static int CARDS_ISSUED; // total number of cards issued to all users
+    private static int CARDS_ISSUED = 0; // total number of cards issued to all users
     private int card_id; // unique identifier
 
     public Card() {
         this.balance = 0.0;
         this.card_id = 0; // How are we going to generate a unique number?
+        CARDS_ISSUED += 1;
+        card_id = CARDS_ISSUED;
     }
 
     public static void main(String[] args) {
@@ -22,4 +24,6 @@ public class Card {
     public void deductFare(int amount) {
         this.balance -= amount;
     }
+
+    public boolean hasBalance() {return this.balance >= 0; }
 }
--- src/transitapp/Trip.java
+++ src/transitapp/Trip.java
@@ -1,5 +1,7 @@
 package transitapp;
 
+import java.lang.Math;
+
 public class Trip {
 
     private static double MAX_COST;
@@ -13,5 +15,9 @@ public class Trip {
 
     public static void main(String[] args) {
 
+    }
+
+    public int stationsTravelled(Location start, Location end) {
+        return Math.abs(Location.getAllLocations().indexOf(start) -  Location.getAllLocations().indexOf(end));
     }
 }
