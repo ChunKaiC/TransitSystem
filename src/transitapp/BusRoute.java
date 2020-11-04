b7d09aedef485fb33c826917b45dfcbbdd30bad6
Kai <kai@chen.com>
2020-11-04 16:12:09 -0500

    Added BusRoute.java

---
+++ src/transitapp/BusRoute.java
@@ -0,0 +1,12 @@
+package transitapp;
+
+public class BusRoute extends TransitRoutes {
+
+	public BusRoute(LocationNode node, double fare) {
+		super(node, fare);
+	}
+	
+    public static void main(String[] args) {
+
+    }
+}
