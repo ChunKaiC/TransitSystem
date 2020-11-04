3f179b4cc11b0cf13bb77ccd7cea9177b18db1e3
Kai <kai@chen.com>
2020-11-04 16:12:44 -0500

    Added SubwayRoute.java

---
+++ src/transitapp/SubwayRoute.java
@@ -0,0 +1,12 @@
+package transitapp;
+
+public class SubwayRoute extends TransitRoutes{
+
+	public SubwayRoute(LocationNode node, double fare) {
+		super(node, fare);
+	}
+	
+    public static void main(String[] args) {
+
+    }
+}
