061f7c1b8d11b623443284482e25090259dffbc2
visanaxel <axel.visan@mail.utoronto.ca>
2020-11-04 15:55:01 -0500

    Updated AdminUser class - Adding/Removing stations/stops

--- src/transitapp/AdminUser.java
+++ src/transitapp/AdminUser.java
@@ -5,7 +5,9 @@ public class AdminUser {
 	private static double totalCost;
 	private static int numBuses;
 	private static double busPrice;
-	// How much does it cost to maintain the busses / subways? How will we keep track of it?
+	private static double busTravelCost; // the cost (expenses) for a bus to go from one stop to another.
+	private static double subwayTravelCost; // the cost (expenses) for a subway to go from one station to another.
+	// For simplicity, the travel expenses will be calculated using the 2 above ^
 	
 	
 	public AdminUser(double totalRev, double totalCost, int numBuses, double busPrice) {
@@ -24,5 +26,21 @@ public class AdminUser {
 	public static void purchaseBuses(int numBuses) {
 		AdminUser.numBuses += numBuses;
 		totalCost += numBuses * busPrice;
+	}
+
+	public static void addStop(Stop stop, BusRoute route, int index) {
+		// add a stop to the bus route
+	}
+
+	public static void removeStop(Stop stop, BusRoute route, int index) {
+		// remove a stop from the bus route
+	}
+
+	public static void addStation(Station station, int index) {
+		// add a station to the subway route
+	}
+
+	public static void removeStation(Station station, int index) {
+		// remove station from the subway
 	}
 }
