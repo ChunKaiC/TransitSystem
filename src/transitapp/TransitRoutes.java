75315b271908bdf9d2c64b148b75208f5b27ab67
Kai <kai@chen.com>
2020-11-05 17:47:19 -0500

    Implemented getters and setters for TransitRoutes

--- src/transitapp/TransitRoutes.java
+++ src/transitapp/TransitRoutes.java
@@ -2,11 +2,13 @@ package transitapp;
 
 import java.util.ArrayList;
 
-public class TransitRoutes {
-	private ArrayList<Object> route;
+public abstract class TransitRoutes {
+	private String name;
+	private ArrayList<Location> route;
 	private double fare;
 	
-	public TransitRoutes(ArrayList<Object> route, double fare) {
+	public TransitRoutes(String name, ArrayList<Location> route, double fare) {
+		this.name = name;
 		this.route = route;
 		this.fare = fare;
 	}
@@ -14,7 +16,26 @@ public class TransitRoutes {
 	public double getFare() {
 		return this.fare;
 	}
-
+	
+	public void setFare(double newFare) {
+		this.fare = newFare;
+	}
+	
+	public ArrayList<Location> getRoute(){
+		return this.route;
+	}
+	
+	public void setRoute(ArrayList<Location> newRoute){
+		this.route = newRoute;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
 	
     public static void main(String[] args) {
 
