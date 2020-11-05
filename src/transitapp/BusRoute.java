2ac79ab4854133246026303db13608abecd00485
Kai <kai@chen.com>
2020-11-05 17:48:22 -0500

    Updated the BusRoute class

--- src/transitapp/BusRoute.java
+++ src/transitapp/BusRoute.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 
 public class BusRoute extends TransitRoutes {
 
-	private ArrayList<Stop> currRoute;
-
-	public BusRoute(LocationNode node, double fare) {
-		super(node, fare);
+	public BusRoute(String name, ArrayList<Location> route, double fare) {
+		super(name, route, fare);
 	}
 	
     public static void main(String[] args) {
