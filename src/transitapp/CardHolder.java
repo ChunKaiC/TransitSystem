0d91f327f06d69134ab3def1aa2c09b4126b1267
visanaxel <axel.visan@mail.utoronto.ca>
2020-11-08 15:55:56 -0500

    CardHolder now accounts for the cases: not tapping on or off

--- src/transitapp/CardHolder.java
+++ src/transitapp/CardHolder.java
@@ -56,35 +56,49 @@ public class CardHolder {
     }
 
     public boolean tapOn(Location location, int card_id, TransitRoutes route) {
-        this.onRoute = true;
-        this.tapOnLocation = location;
-        this.currTrip = new Trip();
-        this.currTrip.addLocation(location);
         Card current_card = cards.get(card_id); // Must be able to get card from the list based on its id.
+        this.currTrip = new Trip();
+
+        if (tapOnLocation != null) {
+            // this means the person never tapped off (previous trip)
+            // deduct max cost
+            current_card.deductFare(this.currTrip.getMaxCost());
+
+            // so at this point, the person may not have balance to tap on again
+        }
 
         if (current_card.hasBalance()) {
+            this.onRoute = true;
+            this.tapOnLocation = location;
+            this.currTrip.addLocation(location);
+
             // For bus
             if (location instanceof Stop) {
                 current_card.deductFare(route.getFare());
             }
 
-            // For subway
-            else if (location instanceof Station) {
-                this.tapOnLocation = location;
-                current_card.deductFare(route.getFare());
-            }
             return true;
         }
         return false;
     }
 
     public void tapOff(Station location, int card_id, TransitRoutes route) {
+        // Only for subway stations
         this.onRoute = false;
         this.currTrip.addLocation(location);
         Card current_card = findCard(this.cards, card_id); // Must be able to get card from the list based on its id.
-            double amount = this.currTrip.stationsTravelled(this.tapOnLocation, location, route);
-            double cost = amount * route.getFare();
-            if (cost > this.currTrip.getMaxCost()) {
+            double numStations = this.currTrip.stationsTravelled(this.tapOnLocation, location, route);
+            double cost = numStations * route.getFare();
+
+
+
+            if (tapOnLocation == null) {
+                // The cardholder never tapped on, so we will charge them the max cost
+                cost = route.getRoute().indexOf(location) * route.getFare();
+            }
+
+            if (cost > this.currTrip.getMaxCost())
+            {
                 cost = this.currTrip.getMaxCost();
             }
             current_card.deductFare(cost);
