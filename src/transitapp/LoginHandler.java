358a0ac2094a5b52090241efc66e893b27a05bee
Faisal Masalha <faisal.masalha@mail.utoronto.ca>
2020-11-09 21:09:46 -0500

    Updated handler

--- src/transitapp/LoginHandler.java
+++ src/transitapp/LoginHandler.java
@@ -4,10 +4,22 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Labeled;
+import javafx.scene.control.TextField;
 
-public class TransitHandler implements EventHandler<ActionEvent>{
+public class LoginHandler implements EventHandler<ActionEvent>{
 
+	private TextField name;
+	private TextField email;
+	private TextField adminId;
 	
+	public LoginHandler(TextField name, TextField email) {
+		this.name = name;
+		this.email = email;
+	}
+	
+	public LoginHandler(TextField adminId) {
+		this.adminId = adminId;
+	}
 	
 	@Override
 	public void handle(ActionEvent arg0) {
