7a2f2682034151381c4c3917ee87cfa0e4db5a06
Faisal Masalha <faisal.masalha@mail.utoronto.ca>
2020-11-14 17:15:51 -0500

    Updated constructor

--- src/transitapp/StartTripHandler.java
+++ src/transitapp/StartTripHandler.java
@@ -1,26 +1,47 @@
 package transitapp;
 
+import java.io.FileNotFoundException;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 
 public class StartTripHandler implements EventHandler<ActionEvent>{
 
 	private ComboBox<Card> cardList;
 	private ComboBox<Location> lList;
+	private TransitGui obj;
+	private CardHolder user;
+	private Stage stage;
 
-	public StartTripHandler(ComboBox<Card> cardList, ComboBox<Location> list) {
+	public StartTripHandler(Stage stage, CardHolder user, ComboBox<Card> cardList, ComboBox<Location> list, TransitGui transitGui) {
 		// TODO Auto-generated constructor stub
 		this.cardList = cardList;
 		this.lList = list;
+		this.obj = transitGui;
+		this.user = user;
+		this.stage = stage;
 		//System.out.println(cardList.getValue());
 	}
 
 	@Override
 	public void handle(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		System.out.println(this.cardList.getValue());
-		System.out.println(this.lList.getValue());
+		//System.out.println(((Button) arg0.getSource()).getText());
+		//System.out.println(this.lList.getValue());
+		if (((Button) arg0.getSource()).getText().equals("Start Trip")) {
+			// pass in location chosen and card chosen 
+			Card selectedCard = this.cardList.getValue();
+			Location start = this.lList.getValue();
+			try {
+				this.obj.continueTrip(this.stage, selectedCard, start, this.user);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
